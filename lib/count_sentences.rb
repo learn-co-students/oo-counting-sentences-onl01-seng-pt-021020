a767b408ee3c35ddde3d01331ed4d6654b2ca3a3
Daniel Sasser <danielbsasser@gmail.com>
2020-02-16 16:45:24 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentence_array = self.split(/[.!?]/)
+      sentence_array.each_with_index do |sentence, i|
+        if sentence.length <= 2
+          sentence_array.delete_at(i)
+        end
+      end
+    sentence_array.length
   end
 end
