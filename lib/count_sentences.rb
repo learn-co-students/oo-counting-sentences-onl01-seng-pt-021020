555da5acf03e5d7da82ca0bf3cc153f3361364c1
Steven Riggs <sfriggs21@gmail.com>
2020-02-20 11:07:25 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    count = 0
+    first_split = self.split('.')
+    for first_phrase in first_split do
+      second_split = first_phrase.split('?')
+      for second_phrase in second_split do
+        third_split = second_phrase.split('!')
+        for phrase in third_split do
+          if phrase.length > 0
+            count += 1
+          end
+        end
+      end
+    end
+    count
   end
 end
