929af2f31f5fa54a7e990df6a6047b7371e2837a
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-02-27 18:48:20 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,43 @@
-require 'pry'
+
 
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true 
+    else 
+      false
+    end
   end
 
-  def question?
+    
 
+  def question?
+    if self.end_with?("?")
+      true 
+    else 
+      false
+    end 
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true 
+    else 
+      false
+    end
   end
 
   def count_sentences
+    split_array = self.split(/[.?!]/).reject {
+        |sentence| sentence.empty?
+    }
 
+    #We split on any and all periods, question marks, and exclamation points using // 
+    #Then, we rejected all empty strings by iterating over every element in the split array and removing any/all empty strings. 
+
+
+    return split_array.length
   end
+
 end
