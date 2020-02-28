eb76278dae0eb3e656b6800664b28298242f593b
tyler jones <rteyl1690@gmail.com>
2020-02-28 03:33:05 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    new_str = self.split(/[.!?]/)
+    new_str.delete_if do |word| word.length == 0 end
+    new_str.count
   end
 end
